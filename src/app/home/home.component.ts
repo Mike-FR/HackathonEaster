4f1bf66097da7212419225e866c2b5b1713c812d
Frederic Lacrabere <f.lacrabere@gmail.com>
2019-04-18 21:05:52 +0200

    easter egg suite : icone, curseur, maggie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  src = false
+  @ViewChild('videoPlayer') videoplayer: ElementRef;
 
+toggleVideo(event: any) {
+    this.videoplayer.nativeElement.play();
+}
+maggieClicked(){
+return this.src = true
+}
   constructor() { }
 
   ngOnInit() {
