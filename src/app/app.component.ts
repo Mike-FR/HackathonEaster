1a6ead9883ef8963cf03f1e05afd6355088e4c1c
Frederic Lacrabere <f.lacrabere@gmail.com>
2019-04-18 16:42:36 +0200

    premier ajout de l'easter egg vidéo + souris + favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathonEasterEgg';
+ src = false
+  @ViewChild('videoPlayer') videoplayer: ElementRef;
+
+toggleVideo(event: any) {
+    this.videoplayer.nativeElement.play();
+}
+maggieClicked(){
+return this.src = true
+}
 }
